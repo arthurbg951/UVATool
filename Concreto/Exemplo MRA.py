399f3ee35b04fa8ae4bf17411dbd1e110804686b
victtordrigues <victtordrigues@outlook.com>
2022-03-26 13:08:42 -0300

    Correção na matriz de rigidez [k], (ft4), e foi adicionados comentários no código

--- Concreto/Exemplo MRA.py
+++ Concreto/Exemplo MRA.py
@@ -3,11 +3,11 @@ import numpy as np
 
 '''
 Resolvendo o Exemplo do material pelo método da Rigidez Analítica com Python
-'''
-'''
+
+
 Passo 1: Criando as matrizes e vetores
 '''
-# Matriz de equilibrio - ( L )
+# Matriz de equilibrio - [ L ]
 
 af12 = 0 * (m.pi/180.)
 af23 = 0 * (m.pi/180.)
@@ -36,13 +36,13 @@ lo_eq = np.array([
     [0,         0,        0,    0,        0,       0,   s3,  c3/l34, -c3/l34],
     [0,         0,        0,    0,        0,       0,    0,       0,      -1]
 ])
-# Colocando as restrições de apoio em ( L )
+# Colocando as restrições de apoio em [ L ]
 leq = np.delete(lo_eq, [0, 1, 10], 0)
 # print(leq.shape)
 
-# Matriz de equilibrio Transposta - ( L.T )
+# Matriz de equilibrio Transposta - [ L.T ]
 leqt = lo_eq.transpose()
-# Colocando as restrições de apoio em ( L.T )
+# Colocando as restrições de apoio em [ L.T ]
 leqt = np.delete(leqt, [0, 1, 10], 1)
 # print(leqt.shape)
 """
@@ -50,7 +50,7 @@ leqt = np.delete(leqt, [0, 1, 10], 1)
  1 -> coluna
 """
 
-# Matriz de rigidez do elemento - ( k )
+# Matriz de rigidez do elemento - [ k ]
 
 p11, p12, p22, p23, p33, p34 = 0, 0, 0, 0, 0, 0
 e12, e23, e34 = 205e6, 205e6, 205e6
@@ -59,26 +59,29 @@ aa12, aa23, aa34 = 0.32, 0.32, 0.32
 
 ft1_1 = (3 * p11 / (4 - p11 * p12)) * (4 * e12 * in12 / l12)
 ft2_1 = (3 * p11 * p12 / (4 - p11 * p12)) * (-2 * e12 * in12 / l12)
-ft3_1 = e12 * aa12 / l12
+ft3_1 = (3 * p12 / (4 - p11 * p12)) * (4 * e12 * in12 / l12)
+ft4_1 = e12 * aa12 / l12
 
 ft1_2 = (3 * p22 / (4 - p22 * p23)) * (4 * e23 * in23 / l23)
 ft2_2 = (3 * p22 * p22 / (4 - p22 * p23)) * (-2 * e23 * in23 / l23)
-ft3_2 = e23 * aa23 / l23
+ft3_2 = (3 * p23 / (4 - p22 * p23)) * (4 * e23 * in23 / l23)
+ft4_2 = e23 * aa23 / l23
 
 ft1_3 = (3 * p33 / (4 - p33 * p34)) * (4 * e34 * in34 / l34)
 ft2_3 = (3 * p33 * p34 / (4 - p33 * p34)) * (-2 * e34 * in34 / l34)
-ft3_3 = e34 * aa34 / l34
+ft3_3 = (3 * p34 / (4 - p33 * p34)) * (4 * e34 * in34 / l34)
+ft4_3 = e34 * aa34 / l34
 
 k_el = np.array([
     [ft1_1, ft2_1,     0,     0,     0,     0,     0,     0,     0],
-    [ft2_1, ft1_1,     0,     0,     0,     0,     0,     0,     0],
-    [0,         0, ft3_1,     0,     0,     0,     0,     0,     0],
+    [ft2_1, ft3_1,     0,     0,     0,     0,     0,     0,     0],
+    [0,         0, ft4_1,     0,     0,     0,     0,     0,     0],
     [0,         0,     0, ft1_2, ft2_2,     0,     0,     0,     0],
-    [0,         0,     0, ft2_2, ft1_2,     0,     0,     0,     0],
-    [0,         0,     0,     0,     0, ft3_2,     0,     0,     0],
+    [0,         0,     0, ft2_2, ft3_2,     0,     0,     0,     0],
+    [0,         0,     0,     0,     0, ft4_2,     0,     0,     0],
     [0,         0,     0,     0,     0,     0, ft1_3, ft2_3,     0],
-    [0,         0,     0,     0,     0,     0, ft2_3, ft1_3,     0],
-    [0,         0,     0,     0,     0,     0,     0,     0, ft3_3]
+    [0,         0,     0,     0,     0,     0, ft2_3, ft3_3,     0],
+    [0,         0,     0,     0,     0,     0,     0,     0, ft4_3]
 ])
 
 '''
@@ -90,56 +93,67 @@ k_global = leq * k_el * leqt
 print(k_global)
 
 
-# Vetor das Cargas Nodais (Cargas Pontuais e Cargas de Momento) - λ
+# Vetor das Cargas Nodais (Cargas Pontuais e Cargas de Momento) - { λ }
 f1x, f2y, f3m, f4x, f5y, f6m, f7x, f8y, f9m, f10x, f11y, f12m = 0, 0, 0, 50, -10, 0, 20, -30, 0, 0, 0, 0
 force = np.array([f1x, f2y, f3m, f4x, f5y, f6m,
                  f7x, f8y, f9m, f10x, f11y, f12m])
 fn = np.transpose(force)  # Transformando em vetor
 
-# Colocando as restrições de apoio em ( delta )
+# Colocando as restrições de apoio em { λ }
 fn = np.delete(fn, [0, 1, 10], 0)
 # print(fn)
 
 # esforcos, residuals, rank, s = np.linalg.lstsq(leq, fn)
 # print(esforcos)
 
+'''
+        ESTÁTICA
+Relação de Equilíbrio -> {Cargas Nodais} = [Matriz de equilíbrio] * {Esforços Internos}
+
+    {λ} = [L] * {m}
+'''
+
 esforcos = np.linalg.solve(leq, fn)
 # print(esforcos)
+
+# Organizando o vetor resposta
 convertido = []
 
+# Elemento 1
 convertido.append(f'm1 = {esforcos[1]:.2f}')
 convertido.append(f'm2 = {esforcos[2]:.2f}')
 convertido.append(f'n1 = {esforcos[0]:.2f}')
-
+# Elemento 2
 convertido.append(f'm3 = {esforcos[4]:.2f}')
 convertido.append(f'm4 = {esforcos[5]:.2f}')
 convertido.append(f'n2 = {esforcos[3]:.2f}')
-
+# Elemento 3
 convertido.append(f'm5 = {esforcos[7]:.2f}')
 convertido.append(f'm6 = {esforcos[8]:.2f}')
 convertido.append(f'n3 = {esforcos[6]:.2f}')
 
+# Vetor dos esforços internos {m} organizado:
 for i in range(len(convertido)):
         print(convertido[i])
 
 
-# Vetor dos Deslocamentos Nodais (Deslocamentos Lineares e Rotacionais) - δ
+# Vetor dos Deslocamentos Nodais (Deslocamentos Lineares e Rotacionais) - { δ }
 dt1x, dt2y, dr3m, dt4x, dt5y, dr6m, dt7x, dt8y, dr9m, dt10x, dt11y, dr12m = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 dt = np.array([dt1x, dt2y, dr3m, dt4x, dt5y, dr6m,
               dt7x, dt8y, dr9m, dt10x, dt11y, dr12m])
 dlt = np.transpose(dt)  # Transformando em vetor
-# Colocando as restrições de apoio em ( delta )
+# Colocando as restrições de apoio em ( δ )
 dlt = np.delete(dlt, [0, 1, 10], 0)
 # print(dlt.shape)
 
-# Vetor das Deformações Correspondentes (Deformações Lineares e Angulares) - θ
+# Vetor das Deformações Correspondentes (Deformações Lineares e Angulares) - { θ }
 theta1, theta2, delta1, theta3, theta4, delta2, theta5, theta6, delta3 = 0, 0, 0, 0, 0, 0, 0, 0, 0
 tta = np.array([theta1, theta2, delta1, theta3,
                theta4, delta2, theta5, theta6, delta3])
 tta = np.transpose(tta)
 # print(tta.shape)
 
-# Vetor dos Esforços Seccionais Internos (Momentos Fletores e Esforços Normais) - m
+# Vetor dos Esforços Seccionais Internos (Momentos Fletores e Esforços Normais) - { m }
 m1, m2, n1, m3, m4, n2, m5, m6, n3 = 0, 0, 0, 0, 0, 0, 0, 0, 0
 esf_in = np.array([m1, m2, n1, m3, m4, n2, m5, m6, n3])
 # esf_in = np.linalg.solve(k_global, fn)
