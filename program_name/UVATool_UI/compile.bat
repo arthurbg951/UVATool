25e84a5ae9dd0e3f30333393a62ead30ee53f03d
arthur brito <arthurbg951@hotmail.com>
2022-04-22 05:08:19 -0300

    Atualizando compilação no windows

--- program_name/UVATool_UI/compile.bat
+++ program_name/UVATool_UI/compile.bat
@@ -8,10 +8,25 @@ set BUILD_DIRECTORY=build
 set FILE_NAME=form_uvatool
 set UI_FILE=%FILE_NAME%.ui
 SET OUTPUT_FILE=%BUILD_DIRECTORY%\%FILE_NAME%.py
+set ICONS=.\dist\%FILE_NAME%\icons\
 
-@REM pyinstaller -onefile %OUTPUT_FILE% -windowed --icon=.\icons\UVATool_main_window.png
-pyinstaller --onefile --icon=.\icons\UVATool_main_window.png %OUTPUT_FILE% --windowed
+echo EXECUTING: pyinstaller --onefile --icon=.\icons\UVATool_main_window.png %OUTPUT_FILE% --windowed
 
-copy .\icons\* .\dist\icons
+@REM --onefile make the program binary slow - preffer the normal output
+@REM TO USE THIS COMMAND, UNCOMENT ALL LINES
+@REM set ICONS=.\dist\icons\
+@REM pyinstaller --onefile --icon=.\icons\UVATool_main_window.png %OUTPUT_FILE% --windowed
+
+echo EXECUTING: pyinstaller --icon=.\icons\UVATool_main_window.png %OUTPUT_FILE% --windowed
+
+pyinstaller --icon=.\icons\UVATool_main_window.png %OUTPUT_FILE% --windowed
+
+ECHO EXECUTING: set ICONS=.\dist\icons\
+
+ECHO EXECUTING: mkdir %ICONS%
+mkdir %ICONS%
+
+echo EXECUTING: copy .\icons\* %ICONS%
+copy .\icons\* %ICONS%
 
 echo SCRIPT ENDING...
