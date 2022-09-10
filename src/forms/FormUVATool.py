8c15a680ae073bf2e60f0bd4476f14069c9300e9
Victtor Rodrigues <victtordrigues@outlook.com>
2022-09-10 19:03:38 -0300

    Treliça MBT

--- src/forms/FormUVATool.py
+++ src/forms/FormUVATool.py
@@ -334,7 +334,8 @@ class UVAGraphicsScene(QGraphicsScene):
         # self.porticosSucessivos()
         # self.modelotcc()
         # self.momentorotacaoVigaBalanco()
-        self.momentorotacaoPortico()
+        # self.momentorotacaoPortico()
+        self.modeloBielasETirantes()
 
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         if event.button() == Qt.MouseButton.LeftButton:
@@ -683,7 +684,6 @@ class UVAGraphicsScene(QGraphicsScene):
         for element in elements:
             self.drawElement(element)
 
-
     def momentorotacaoPortico(self):
         # Materiais ----------------------------------------------------------------------------------
         secao_p = Rectangle(0.2 * 10, 0.2 * 10)  # hy x hx
@@ -729,6 +729,94 @@ class UVAGraphicsScene(QGraphicsScene):
         for element in elements:
             self.drawElement(element)
 
+    def modeloBielasETirantes(self):
+        # Materiais ----------------------------------------------------------------------------------
+        secao_p = Rectangle(1, 1)  # hy x hx
+        area_p = secao_p.area
+        inercia_p = secao_p.inertia
+        secao_v = Rectangle(1, 1) # base x altura
+        area_v = secao_v.area
+        inercia_v = secao_v.inertia
+        young = 1
+
+        # Nós ----------------------------------------------------------------------------------------
+        n1 = NodeDraw(0 * 10, -0 * 10)
+        n2 = NodeDraw(10 * 10, -0 * 10)
+        n3 = NodeDraw(20 * 10, -0 * 10)
+        n4 = NodeDraw(30 * 10, -0 * 10)
+        n5 = NodeDraw(60 * 10, -0 * 10)
+        n6 = NodeDraw(70 * 10, -0 * 10)
+        n7 = NodeDraw(80 * 10, -0 * 10)
+        n8 = NodeDraw(90 * 10, -0 * 10)
+        
+        n9 = NodeDraw(10 * 10, -11.7 * 10)
+        n10 = NodeDraw(20 * 10, -11.7 * 10)
+        n11 = NodeDraw(30 * 10, -11.7 * 10)
+        n12 = NodeDraw(60 * 10, -11.7 * 10)
+        n13 = NodeDraw(70 * 10, -11.7 * 10)
+        n14 = NodeDraw(80 * 10, -11.7 * 10)
+
+
+        # Apoios -------------------------------------------------------------------------------------
+        n1.setSupport(Apoio.segundo_genero)
+        n8.setSupport(Apoio.primeiro_genero)
+
+        # Fator Pi -----------------------------------------------------------------------------------
+        n2.setP(1e-32)
+        n3.setP(1e-32)
+        n4.setP(1e-32)
+        n5.setP(1e-32)
+        n6.setP(1e-32)
+        n7.setP(1e-32)
+        
+        n9.setP(1e-32)
+        n10.setP(1e-32)
+        n11.setP(1e-32)
+        n12.setP(1e-32)
+        n13.setP(1e-32)
+        n14.setP(1e-32)
+
+        # Cargas -------------------------------------------------------------------------------------
+        n11.setNodalForce(NodalForce(0, -10e3, 0))
+        n12.setNodalForce(NodalForce(0, -10e3, 0))
+
+        # --------------------------------------------------------------------------------------------
+        e1 = ElementDraw(n1, n2, area_p, inercia_p, young)
+        e2 = ElementDraw(n2, n3, area_v, inercia_v, young)
+        e3 = ElementDraw(n3, n4, area_v, inercia_v, young)
+        e4 = ElementDraw(n4, n5, area_v, inercia_v, young)
+        e5 = ElementDraw(n5, n6, area_p, inercia_p, young)
+        e6 = ElementDraw(n6, n7, area_p, inercia_p, young)
+        e7 = ElementDraw(n7, n8, area_p, inercia_p, young)
+        
+        e8 = ElementDraw(n9, n10, area_p, inercia_p, young)
+        e9 = ElementDraw(n10, n11, area_p, inercia_p, young)
+        e10 = ElementDraw(n11, n12, area_p, inercia_p, young)
+        e11 = ElementDraw(n12, n13, area_p, inercia_p, young)
+        e12 = ElementDraw(n13, n14, area_p, inercia_p, young)
+        
+        e13 = ElementDraw(n1, n9, area_p, inercia_p, young)
+        e14 = ElementDraw(n2, n10, area_p, inercia_p, young)
+        e15 = ElementDraw(n3, n11, area_p, inercia_p, young)
+        e16 = ElementDraw(n6, n12, area_p, inercia_p, young)
+        e17 = ElementDraw(n7, n13, area_p, inercia_p, young)
+        e18 = ElementDraw(n8, n14, area_p, inercia_p, young)
+        
+        e19 = ElementDraw(n2, n9, area_p, inercia_p, young)
+        e20 = ElementDraw(n3, n10, area_p, inercia_p, young)
+        e21 = ElementDraw(n4, n11, area_p, inercia_p, young)
+        e22 = ElementDraw(n5, n12, area_p, inercia_p, young)
+        e23 = ElementDraw(n6, n13, area_p, inercia_p, young)
+        e24 = ElementDraw(n7, n14, area_p, inercia_p, young)
+
+        nodes = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14]
+        elements = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18,
+                    e19, e20, e21, e22, e23, e24]
+
+        for node in nodes:
+            self.drawNode(node)
+        for element in elements:
+            self.drawElement(element)
 
 
 '''
