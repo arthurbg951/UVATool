20b473a980016f0be924d7b7bd4d2b58200253a8
arthurbg951 <arthurbg951@hotmail.com>
2022-12-29 01:01:15 -0300

    Adicionando Porticos sucessivos ao paint test timing

--- tests/exemples/Multiple Structures Timing.py
+++ tests/exemples/Multiple Structures Timing.py
@@ -235,6 +235,59 @@ def semi_rigido3():
     return structure
 
 
+def porticosSucessivos(n_andares=20, n_pilares_por_andar=7, distancia_pilares=6, pe_direito=3):
+    '''PORTICOS SUCESSIVOS'''
+    structure_name = 'PORTICOS SUCESSIVOS'
+    # CONDIÇÕES INICIAIS
+    n_andares = n_andares + 1
+    # n_pilares_por_andar = 7
+    # distancia_pilares = 6
+    # pe_direito = 3
+
+    # SEÇÃO DOS PILARES
+    rec = Rectangle(0.20, 0.20)
+    area_p = rec.area
+    inercia_p = rec.inertia
+
+    # SEÇÃO DAS VIGAS
+    rec = Rectangle(0.15, 0.60)
+    area_v = rec.area
+    inercia_v = rec.inertia
+
+    # CONCRETO 30 MPa
+    young_modulus = 27_000_000_000
+
+    nodes: list[Node] = []
+    elements: list[Element] = []
+
+    for x in range(n_pilares_por_andar):
+        for y in range(n_andares):
+            nodes.append(Node(x * distancia_pilares, y * pe_direito))
+
+    # PILARES
+    for pilar in range(n_pilares_por_andar):
+        for i in range(n_andares-1):
+            node_pos = i + pilar * n_andares
+            elements.append(Element(nodes[node_pos], nodes[node_pos + 1], area_p, inercia_p, young_modulus))
+
+    # VIGAS
+    for qtpilares in range(1, n_pilares_por_andar, 1):
+        for i in range(1, n_andares, 1):
+            elements.append(Element(nodes[i + (qtpilares - 1) * n_andares], nodes[i + qtpilares * n_andares], area_v, inercia_v, young_modulus))
+
+    # APOIOS DOS PILARES MAIS INFERIORES
+    for i in range(n_pilares_por_andar):
+        nodes[i * n_andares].setSupport(Apoio.terceiro_genero)
+
+    # FORÇA DE VENTO A CADA ANDAR
+    for i in range(1, n_andares, 1):
+        nodes[i].setNodalForce(NodalForce(i * i * 10, 0, 0))
+
+    structure = Structure(structure_name, nodes, elements)
+
+    return structure
+
+
 def process_simulation(structure: Structure,
                        show_equilibrium_matrix=False,
                        show_frame_stiffness_matrix=False,
@@ -294,5 +347,6 @@ if __name__ == "__main__":
     process_simulation(portico_com_vao_de_10m_pe_direito_3m())
     process_simulation(portico_maluco())
     process_simulation(semi_rigido3())
+    process_simulation(porticosSucessivos(n_andares=53, n_pilares_por_andar=2))
 
-    print(f"TOTAL TIME = {(datetime.now()- inicial_time).seconds}s")
+    print(f"TOTAL TIME = {(datetime.now() - inicial_time).seconds}s")
