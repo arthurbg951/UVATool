a73113507ff1ce4321835e43b0f3228764f7e71a
Arthur Brito <arthurbg951@hotmail.com>
2023-09-03 23:49:12 -0300

    Updating comments

--- src/exemples/Multiple Structures Timing.py
+++ src/exemples/Multiple Structures Timing.py
@@ -3,7 +3,8 @@ from UVATool.Enums import *
 from UVATool.Exceptions import *
 from UVATool.Colors import *
 from datetime import datetime
-import math, numpy
+import math
+import numpy
 
 # SEÇÃO
 secao = Rectangle(0.012, 0.001)
@@ -300,22 +301,23 @@ def porticosSucessivos(n_andares=20,
 
     return structure
 
-def process_simulation(structure: Structure,
-                       show_equilibrium_matrix=False,
-                       show_frame_stiffness_matrix=False,
-                       show_global_frame_stiffness=False,
-                       show_nodal_displacement=False,
-                       show_deformations=False,
-                       show_internal_forces=False,
-                       show_processed_time=True,
-                       show_errors=True):
+
+def process_simulation(
+        structure: Structure,
+        show_equilibrium_matrix=False,
+        show_frame_stiffness_matrix=False,
+        show_global_frame_stiffness=False,
+        show_nodal_displacement=False,
+        show_deformations=False,
+        show_internal_forces=False,
+        show_processed_time=True,
+        show_errors=True):
 
     error_msg = to_red('ERRO')
     try:
         erros: list[str] = []
         time = None
-        proc = Process(structure.nodes, structure.elements,
-                       Analise.elastica.viaRigidezAnalitica)
+        proc = Process(structure.nodes, structure.elements, Analise.viaRigidezAnalitica)
         time = proc.getProcessTime()
         plot = Print(proc)
 
@@ -360,6 +362,17 @@ def process_simulation(structure: Structure,
 
 
 if __name__ == "__main__":
+    uvatool_text = '''                                                                                                                          
+888     888 888     888        d8888 88888888888                   888 
+888     888 888     888       d88888     888                       888 
+888     888 888     888      d88P888     888                       888 
+888     888 Y88b   d88P     d88P 888     888      .d88b.   .d88b.  888 
+888     888  Y88b d88P     d88P  888     888     d88""88b d88""88b 888 
+888     888   Y88o88P     d88P   888     888     888  888 888  888 888 
+Y88b. .d88P    Y888P     d8888888888     888     Y88..88P Y88..88P 888 
+ "Y88888P"      Y8P     d88P     888     888      "Y88P"   "Y88P"  888
+ '''
+    print(uvatool_text)
     inicial_time = datetime.now()
 
     process_simulation(balanco())
