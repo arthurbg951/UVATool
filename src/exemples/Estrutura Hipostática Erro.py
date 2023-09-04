9bcdd519d3470b7c914d903c1862dbe2ee70c7aa
Arthur Brito <arthurbg951@hotmail.com>
2023-09-03 23:09:34 -0300

    Fixing structure to open in UI

--- src/exemples/Estrutura Hipostática Erro.py
+++ src/exemples/Estrutura Hipostática Erro.py
@@ -1,34 +1,35 @@
 from UVATool import *
 from UVATool.Enums import *
-import numpy
+from numpy.linalg import LinAlgError
 
-try:
-    n1 = Node(0, 0)
-    n2 = Node(1, 0)
-    n3 = Node(2, 0)
+# DEFININDO NÓS
+n1 = Node(0, 0)
+n2 = Node(1, 0)
+n3 = Node(2, 0)
 
-    n1.setSupport(Apoio.segundo_genero)
-    n2.setSupport(Apoio.rotula)
-    n3.setSupport(Apoio.primeiro_genero)
+# DEFININDO APOIOS
+n1.setSupport(Apoio.segundo_genero)
+n2.setSupport(Apoio.rotula)
+n3.setSupport(Apoio.primeiro_genero)
 
-    n2.setNodalForce(NodalForce(0, -10, 0))
+# DEFININDO FORÇA
+n2.setNodalForce(NodalForce(0, -10, 0))
 
-    e1 = Element(n1, n2, 1, 1, 1)
-    e2 = Element(n2, n3, 1, 1, 1)
+# DEFININDO ELEMENTO
+e1 = Element(n1, n2, 1, 1, 1)
+e2 = Element(n2, n3, 1, 1, 1)
 
-    nodes = [n1, n2, n3]
-    elements = [e1, e2]
+nodes = [n1, n2, n3]
+elements = [e1, e2]
 
-    structure = Structure('ESTRUTURA HIPOSTÁTICA COM ERRO PARA CORREÇÃO', nodes, elements)
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
+    try:
         calc = Process(nodes, elements)
-
         plot = Print(calc)
         plot.internalForces()
-
-except numpy.linalg.LinAlgError as e:
-    print(e.args[0])
-    print(
-        "O ERRO ESTÁ DEVE SER INVESTIGADO PARA DEMONSTRAR QUE A ESTRUTRURA É HIPOSTÁTICA ..."
-    )
+    except LinAlgError as e:
+        print(e.args[0])
+        print("O ERRO DEVE SER INVESTIGADO PARA DEMONSTRAR QUE A ESTRUTRURA É HIPOSTÁTICA ...")
+else:
+    structure = Structure('ESTRUTURA HIPOSTÁTICA COM ERRO PARA CORREÇÃO', nodes, elements)
