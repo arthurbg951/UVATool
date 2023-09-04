53a0fdb0fc0c8468a2cf574abd7ce958fe79f647
Arthur Brito <arthurbg951@hotmail.com>
2023-09-03 23:33:24 -0300

    Updating comments

--- src/exemples/fixed-middle-force.py
+++ src/exemples/fixed-middle-force.py
@@ -1,6 +1,22 @@
 from UVATool import *
 from UVATool.Enums import *
 
+'''
+____    ____  __    _______      ___      
+\   \  /   / |  |  /  _____|    /   \     
+ \   \/   /  |  | |  |  __     /  ^  \    
+  \      /   |  | |  | |_ |   /  /_\  \   
+   \    /    |  | |  |__| |  /  _____  \  
+    \__/     |__|  \______| /__/     \__\ 
+
+ _______ .__   __.   _______      ___           _______..___________.     ___       _______       ___      
+|   ____||  \ |  |  /  _____|    /   \         /       ||           |    /   \     |       \     /   \     
+|  |__   |   \|  | |  |  __     /  ^  \       |   (----``---|  |----`   /  ^  \    |  .--.  |   /  ^  \    
+|   __|  |  . `  | |  | |_ |   /  /_\  \       \   \        |  |       /  /_\  \   |  |  |  |  /  /_\  \   
+|  |____ |  |\   | |  |__| |  /  _____  \  .----)   |       |  |      /  _____  \  |  '--'  | /  _____  \  
+|_______||__| \__|  \______| /__/     \__\ |_______/        |__|     /__/     \__\ |_______/ /__/     \__\ 
+'''
+
 n1 = Node(0, 0)
 n2 = Node(2.5, 0)
 n3 = Node(5, 0)
@@ -14,10 +30,11 @@ e2 = Element(n2, n3, 1, 1, 1)
 nodes = [n1, n2, n3]
 elements = [e1, e2]
 
-structure = Structure('EXEMPLO DE UTILIZAÇÃO', nodes, elements)
 
 if __name__ == "__main__":
     calc = Process(nodes, elements)
 
     results = Print(calc)
     results.nodalDisplacement()
+else:
+    structure = Structure('EXEMPLO DE UTILIZAÇÃO', nodes, elements)
