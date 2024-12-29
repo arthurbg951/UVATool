bda22fafb363cffb0c45c56b7d1b04c3b7246201
Arthur Brito <arthurbg951@hotmail.com>
2024-12-29 06:52:35 -0300

    Organazing uvatool build

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+pkg_names = ['uvatool']
+
+setup(
+    name='UVATool',
+    version='1.0.0',
+    author='Arthur Brito',
+    author_email='arthurbg951@hotmail.com',
+    description='Simula estruturas reticuladas utilizando MNE ou MRA',
+    packages=pkg_names,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+            'Licence :: GPL-2.0',
+            'Operating System :: OS Independent'
+    ],
+    requires=['numpy']
+)
